4d3e375cfe08dc87da7b177191c495233f2c532b
Vbon <5914018@qq.com>
2023-08-16 13:31:24 +0800

    Update pinganAssets.js

--- js/pinganAssets.js
+++ js/pinganAssets.js
@@ -1,1 +1,20 @@
+const resp = {};
+const body = JSON.parse(typeof $response != "undefined" && $response.body || null);
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
 
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
+	delete $request.headers["X-RevenueCat-ETag"];
+	resp.headers = $request.headers;
+} else if (body) {
+	// $notify("解析Json", "", "");
+  const totalAsset = body.data.content.interfaces.totalAsset
+  $notify(totalAsset, "", "");
+  body.data.content.interfaces.totalAsset=totalAsset*20
+	resp.body = JSON.stringify(body);
+}
+
+$done(resp);
+
+
+//https://github.com/Peng-YM/QuanX/tree/master/Tools/OpenAPI
